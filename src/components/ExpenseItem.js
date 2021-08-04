e62cafa0f9da1cd3b2c8ee8e70fd65ef134f0e9a
onrush <t6momd00@students.oamk.fi>
2021-08-04 13:40:46 +0300

    Added stylesheet in the componetnt

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem(){
     return(
-        <div>
+        <div className="expense-item">
             <div>March 28th,2021</div>
-            <div>
+            <div className="expense-item__description">
                 <h2>Home insurance</h2>
-                <div>€175.99</div>
+                <div className="expense-item__price">€175.99</div>
             </div>
         </div>
     );
