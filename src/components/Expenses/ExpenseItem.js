c9fbf2e0af8a6adeadc05de52e3e16f37a061f7a
onrush <t6momd00@students.oamk.fi>
2021-08-05 16:23:46 +0300

    Used hooks as an experiment to change items on the list

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,28 @@
-import ExpenseDate from './ExpenseDate';
-import './ExpenseItem.css';
-import Card from '../UI/Card';
+import React, { useState } from 'react';
 
-function ExpenseItem(props) {
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
+  // function clickHandler() {}
+  const [title, setTitle] = useState(props.title);
+  console.log("expenses by React");
+  
+  const clickHandler = () => {
+    setTitle("worked!");
+    console.log(title);
+  };
+
+  
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change it</button>
     </Card>
   );
 }
