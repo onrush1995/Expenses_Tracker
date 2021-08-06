fb1fd7c40d8fc96b87a32e09aae56e35c08cf18f
onrush <t6momd00@students.oamk.fi>
2021-08-06 10:17:26 +0300

    Added conditional return statement for the filtered year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2021');
@@ -17,22 +17,15 @@ const Expenses = (props) => {
   });
 
   return (
-    <div>
+    <li>
       <Card className='expenses'>
         <ExpensesFilter
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-          {filteredExpenses.map((expense) => (
-            <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        <ExpensesList items={filteredExpenses}/>
       </Card>
-    </div>
+    </li>
   );
 };
 
