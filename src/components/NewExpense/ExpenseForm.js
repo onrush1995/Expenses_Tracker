2068b3c251c16ba861c352f64babca2f3d50b630
onrush <t6momd00@students.oamk.fi>
2021-08-06 10:32:59 +0300

    Cancel button added with functionality

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -6,16 +6,15 @@ const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState('');
   const [enteredAmount, setEnteredAmount] = useState('');
   const [enteredDate, setEnteredDate] = useState('');
-
-
+ 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
- 
+  
   };
 
   const amountChangeHandler = (event) => {
     setEnteredAmount(event.target.value);
-   
+  
   };
 
   const dateChangeHandler = (event) => {
@@ -71,6 +70,7 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className='new-expense__actions'>
+        <button type="button" onClick={props.onCancel}>Cancel</button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
